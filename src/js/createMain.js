4ec954f71c60731aa5ae8699c331bbad4448fd61
Enkoff <0639818413@ukr.net>
2020-12-13 15:22:41 +0200

    init: start covid-19

---
+++ src/js/createMain.js
@@ -0,0 +1,27 @@
+import { root } from './createHeader'
+ 
+export default function createMain() {
+    
+    const mainHtmlTemplate = `
+    <main class="main">
+        <div class="left-content">
+            <div class="global__cases"></div>
+            <div class="cases-country"></div>
+        </div>
+        <div class="map-wrapper">
+            <div class="map"></div>
+        </div>
+        <div class="right-content">
+            <div class="header__content">
+                <div class="deaths"></div>
+                <div class="recovered"></div>
+            </div>
+            <div class="footer__content">
+                <div class="graph"></div>
+            </div>   
+        </div>
+    </main>
+    `
+    root.insertAdjacentHTML('beforeend', mainHtmlTemplate);
+
+}
