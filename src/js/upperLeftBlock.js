c0ad99509a313115c72fcad70bd655af16f28d98
Enkoff <0639818413@ukr.net>
2020-12-21 22:33:48 +0200

    fix: chang import

--- src/js/upperLeftBlock.js
+++ src/js/upperLeftBlock.js
@@ -1,17 +1,18 @@
-import { getData } from './getData';
+import { getDataV3 } from './getData';
 
 export default function getSummary() {
-  const summaryURL = 'summary';
+  const totalURL = 'all';
   const globalTitle = document.querySelector('.global__title');
   const globalConfirmed = document.querySelector('.global__confirmed');
   const globalDate = document.querySelector('.global__date');
 
   globalTitle.textContent = 'Global Cases';
 
-  getData(summaryURL).then((res) => {
-    const dateAndTime = res.Date;
-
-    globalConfirmed.textContent = res.Global.NewConfirmed;
-    globalDate.textContent = dateAndTime.replace('T', ' ').replace('Z', '');
+  getDataV3(totalURL).then((res) => {
+    const dateAndTime = String(new Date(res.updated));
+    const array = dateAndTime.split('');
+    const endIndex = array.indexOf('G');
+    globalConfirmed.textContent = res.cases;
+    globalDate.textContent = dateAndTime.substring(0, endIndex);
   });
 }
